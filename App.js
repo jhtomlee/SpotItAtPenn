935e85ff2d06f324dc7354d263150e7129190b27
Tom Lee <jhltom@seas.upenn.edu>
2019-09-06 20:09:35 -0400

    init

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>SpotIt at Penn!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDMyUWWSyLIxrR51_s64ZAVCcXB5ouA_Qs",
+  authDomain: "spotit-at-penn.firebaseapp.com",
+  databaseURL: "https://spotit-at-penn.firebaseio.com",
+  projectId: "spotit-at-penn",
+  storageBucket: "spotit-at-penn.appspot.com",
+  messagingSenderId: "801657040596",
+  appId: "1:801657040596:web:8eedab76765c94a6eb1541"
+};
